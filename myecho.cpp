277089e926bbc8db34d364739081b9d91ce19610
262882 <2628829@students.wits.ac.za>
2024-02-09 15:33:45 +0000

    Use namespace

--- myecho.cpp
+++ myecho.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 
 int main(int argc, char *argv[] ){  
     // argv is a pointer to a C style array of pointers to character arrays
     // Each character array is null terminated
     char *name = argv[0] + 2;  // Drop the ./ part of the program name
-    std::cout<<"You called "<<name<<":"<<std::endl;
+    cout<<"You called "<<name<<":"<<endl;
     for (int cnt = 1; cnt < argc; cnt++){
-        std::cout<<argv[cnt]<<" ";
-        std::cout<<"-- Stored at "<<&argv[cnt];
-        std::cout<<std::endl;
+        cout<<argv[cnt]<<" ";
+        cout<<"-- Stored at "<<&argv[cnt];
+        cout<<endl;
     }
     return 0;
 }
