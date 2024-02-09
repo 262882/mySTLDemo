2e6aba0fad22d693261d97a8a9c1013c73d9e642
262882 <2628829@students.wits.ac.za>
2024-02-09 16:44:26 +0000

    Add libc comment

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <iostream>  // copy system libc header file here from /usr/include/c++/<version>
+// Kernel headers are found in /usr/include/linux
 
 int main(){
     std::cout<<"Hello World\n";
